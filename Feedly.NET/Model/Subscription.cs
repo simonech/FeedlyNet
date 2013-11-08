9a9a3c0eb0e3f1c00e856bae41ceb71af56a2670
Simone Chiaretta <simone@piyosailing.com>
2013-11-08 15:20:48 +0100

    Made Feed as base class for Subscription

--- Feedly.NET/Model/Subscription.cs
+++ Feedly.NET/Model/Subscription.cs
@@ -5,22 +5,14 @@ using Newtonsoft.Json;
 
 namespace Feedly.NET.Model
 {
-    public class Subscription
+    public class Subscription: Feed
     {
         public Subscription()
         {
-            topics = new List<string>();
             categories=new List<Category>();
         }
-
-        public string id { get; set; }
-        public string title { get; set; }
-        public string website { get; set; }
         public List<Category> categories { get; set; }
         [JsonConverter(typeof(UnixDateTimeConverter))]
         public DateTime updated { get; set; }
-        public double velocity { get; set; }
-        public List<string> topics { get; set; }
-        public string state { get; set; }
     }
 }
